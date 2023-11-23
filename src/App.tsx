0fea3fced526278ee90eb1e54c3b59f9461737a9
lamine785260388 <lamine785260388@gmail.com>
2023-11-23 19:30:30 +0000

    Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent,useState,useEffect} from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
+import PokemonList from './pages/pokemon-list';
   
 const App: FunctionComponent = () => {
 
@@ -10,28 +11,7 @@ useEffect(()=>{
 },[]);
     
  return (
-<div>
-  <h1 className='center'> Pokédex</h1>
-  <div className='container'>
-    <div className='row'>
-      {pokemons.map(({id,name,picture,created})=>(
-          <div className='col s6 m4' key={id}>
-            <div className='card horizontal'>
-              <div className='card-image'>
-                <img src={picture} alt={name} />
-              </div>
-              <div className='card-stacked'>
-                <div className='card-content'>
-                  <p>{name} </p>
-                  <p> <small>{created.toString()}</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-      ))}
-    </div>
-  </div>
-</div>
+<PokemonList />
  )
 }
   
