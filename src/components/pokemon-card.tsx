66cdbd673763a6fcf5a25e76b2b927fcb53213c9
lamine785260388 <lamine785260388@gmail.com>
2023-11-25 00:18:18 +0000

    helpers and hook personnalisé

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
+import formatDate from '../helpers/format-date';
+import formatType from '../helpers/format-type';
 type Props = {
   pokemon: Pokemon,
   borderColor?:string
@@ -14,6 +16,11 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon,borderColor='#009688'}) 
     const hideBorder=()=>{
         setColor('#f5f5f5');  // On remet la bordure en gris
     }
+
+
+   
+
+   
   return (
     <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
       <div className="card horizontal" style={{ borderColor:color }}>
@@ -23,7 +30,12 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon,borderColor='#009688'}) 
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
+            {pokemon.types.map(type=>(
+              <span key={type} className={formatType(type)}>{type}</span>
+            )
+
+            )}
           </div>
         </div>
       </div> 
